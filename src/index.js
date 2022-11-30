fa8f9145eae23eb0b0305170627db9b5ace6cedd
Thales Barreto <trvbarreto@gmail.com>
2022-11-30 15:46:28 -0300

    Código melhorado

--- src/index.js
+++ src/index.js
@@ -28,8 +28,10 @@ function checksExistsUserAccount(request, response, next) {
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
-  if (users.find(user => user.username === username)) {
-    return response.status(400).json({error: "create a new user when username already exists"})
+  const userExists = users.find(user => user.username === username);
+
+  if (userExists) {
+    return response.status(400).json({error: "Username already exists"})
   }
 
   const user = {
@@ -69,34 +71,41 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { user } = request;
   const { title, deadline } = request.body;
+  const { id } = request.params;
 
-  if (!(user.todos.find(todo => todo.id === request.params.id))) {
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
     return response.status(404).json({ error: 'Cannot update a non existing todo'});
   }
   
-  user.todos.find(todo => todo.id === request.params.id).title = title;
-  user.todos.find(todo => todo.id === request.params.id).deadline = new Date(deadline);
+  todo.title = title;
+  todo.deadline = new Date(deadline);
 
-  return response.status(200).send(user.todos.find(todo => todo.id === request.params.id));
+  return response.status(200).send(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   const { user } = request;
+  const { id } = request.params;
 
-  if (!(user.todos.find(todo => todo.id === request.params.id))) {
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
     return response.status(404).json({ error: 'Cannot mark a non existing todo as done'});
   }
   
-  user.todos.find(todo => todo.id === request.params.id).done = true;
+  todo.done = true;
 
-  return response.status(200).send(user.todos.find(todo => todo.id === request.params.id));
+  return response.status(200).send(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { user } = request;
+  const { id } = request.params;
   
   const todoIndex = user.todos.findIndex(todo => {
-    return todo.id === request.params.id;
+    return todo.id === id;
   });
 
   if (todoIndex === -1) {
